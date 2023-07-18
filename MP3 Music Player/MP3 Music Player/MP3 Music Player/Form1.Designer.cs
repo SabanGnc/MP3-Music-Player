5c1eb6c61e5bd75b11114cb2774a6aba5fa4941e
Şaban Birkan Genç <139702707+SabanGnc@users.noreply.github.com>
2023-07-18 15:50:06 +0300

    Add files via upload

---
+++ MP3 Music Player/MP3 Music Player/MP3 Music Player/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MP3_Music_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderColor = System.Drawing.Color.PaleGreen;
+            this.guna2Button1.BorderRadius = 15;
+            this.guna2Button1.BorderThickness = 2;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.LightSeaGreen;
+            this.guna2Button1.Font = new System.Drawing.Font("LEMON MILK Bold", 13F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(40, 276);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(180, 108);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "P L A Y";
+            this.guna2Button1.UseTransparentBackground = true;
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.Animated = true;
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.BorderColor = System.Drawing.Color.PaleGreen;
+            this.guna2Button2.BorderRadius = 15;
+            this.guna2Button2.BorderThickness = 2;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.LightSeaGreen;
+            this.guna2Button2.Font = new System.Drawing.Font("LEMON MILK Bold", 13F, System.Drawing.FontStyle.Bold);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(253, 276);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(180, 108);
+            this.guna2Button2.TabIndex = 0;
+            this.guna2Button2.Text = "S T O P";
+            this.guna2Button2.UseTransparentBackground = true;
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.listBox1.DisplayMember = "0";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(702, 276);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 212);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.Animated = true;
+            this.guna2Button3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button3.BorderColor = System.Drawing.Color.PaleGreen;
+            this.guna2Button3.BorderRadius = 15;
+            this.guna2Button3.BorderThickness = 2;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.LightSeaGreen;
+            this.guna2Button3.Font = new System.Drawing.Font("LEMON MILK Bold", 13F, System.Drawing.FontStyle.Bold);
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.Location = new System.Drawing.Point(469, 276);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(180, 108);
+            this.guna2Button3.TabIndex = 0;
+            this.guna2Button3.Text = "R E S U M E";
+            this.guna2Button3.UseTransparentBackground = true;
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(919, 534);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+    }
+}
+
---
+++ MP3 Music Player/MP3 Music Player/MP3 Music Player/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+
+namespace MP3_Music_Player
+{
+    public partial class Form1 : Form
+    {
+
+        private List<string> musicFiles;
+        private SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+            musicFiles = new List<string>();
+            player = new SoundPlayer();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Formun ekranın ortasında konumlanması için gereken hesaplamaları yap
+            int x = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+            int y = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+
+            // Formun konumunu güncelle
+            this.Location = new Point(x, y);
+
+            //-------------------------------------------
+
+            // Formun adını değiştir
+            this.Text = "MP3 Music Player";
+
+            //-------------------------------------------
+
+            // Müzik dosyalarını listeye yükle
+            LoadMusicFiles();
+        }
+
+        private void LoadMusicFiles()
+        {
+            string musicDirectory = @"C:\sounds\Folder"; // Müzik dosyalarının bulunduğu dizin
+            string[] files = Directory.GetFiles(musicDirectory, "*.mp3"); // Sadece MP3 dosyalarını yükle (İsteğe bağlı)
+
+            musicFiles.AddRange(files);
+            listBox1.DataSource = musicFiles; // Listbox'a müzik dosyalarını ata
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Listbox'ta seçili olan müzik dosyasını oynat
+            string selectedMusic = listBox1.SelectedItem.ToString();
+            player.SoundLocation = selectedMusic;
+            player.Play();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            // Oynat düğmesine basıldığında seçili müziği oynat
+            if (listBox1.SelectedItem != null)
+            {
+                player.Play();
+            }
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                player.Stop(); // Müziği duraklatmak için Stop yöntemini kullan
+            }
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                player.Play(); // Duraklatılan müziği tekrar başlatmak için Play yöntemini kullan
+            }
+        }
+    }
+
+}
